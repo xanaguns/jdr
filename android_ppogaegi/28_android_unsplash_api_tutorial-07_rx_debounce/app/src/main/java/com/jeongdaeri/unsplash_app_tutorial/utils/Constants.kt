0aa0dd8f2e96f7065d9523c83cce56de3e1269d0
gschoi <gunsoo.choi@bluebirdcorp.com>
2022-02-04 22:43:29 +0900

    add 28_android_unsplash_api_tutorial-07_rx_debounce project

---
+++ android_ppogaegi/28_android_unsplash_api_tutorial-07_rx_debounce/app/src/main/java/com/jeongdaeri/unsplash_app_tutorial/utils/Constants.kt
@@ -0,0 +1,29 @@
+package com.jeongdaeri.unsplash_app_tutorial.utils
+
+object Constants {
+    const val TAG : String = "로그"
+}
+
+enum class SEARCH_TYPE {
+    PHOTO,
+    USER
+}
+
+enum class RESPONSE_STATUS {
+    OKAY,
+    FAIL,
+    NO_CONTENT
+}
+
+
+object API {
+    const val BASE_URL : String = "https://api.unsplash.com/"
+
+    // 여러분꺼 하셔야됩니다!! ㅎㅎ;;
+    //const val CLIENT_ID : String = "YS7sdqX2kuYBOifsupK1A-J2S4tkMveczqAQVOEBJMs"
+    const val CLIENT_ID : String = "8u7d0TEBwibuDEfuHpsdvwMijRosE40n8a71QBTNWKE"
+
+    const val SEARCH_PHOTOS : String = "search/photos"
+    const val SEARCH_USERS : String = "search/users"
+
+}
---
+++ android_ppogaegi/28_android_unsplash_api_tutorial-07_rx_debounce/app/src/main/java/com/jeongdaeri/unsplash_app_tutorial/utils/Extensions.kt
@@ -0,0 +1,56 @@
+package com.jeongdaeri.unsplash_app_tutorial.utils
+
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+// 문자열이 제이슨 형태인지
+fun String?.isJsonObject():Boolean {
+    if(this?.startsWith("{") == true && this.endsWith("}")){
+        return true
+    } else {
+        return false
+    }
+//    return this?.startsWith("{") == true && this.endsWith("}")
+}
+//fun String?.isJsonObject():Boolean = this?.startsWith("{") == true && this.endsWith("}")
+
+// 문자열이 제이슨 배열인지
+fun String?.isJsonArray() : Boolean {
+    if(this?.startsWith("[") == true && this.endsWith("]")){
+        return true
+    } else {
+        return false
+    }
+}
+
+
+// 날짜 포맷
+fun Date.toSimpleString() : String {
+    val format = SimpleDateFormat("HH:mm:ss")
+    return format.format(this)
+}
+
+
+
+// 에딧 텍스트에 대한 익스텐션
+fun EditText.onMyTextChanged(completion: (Editable?) -> Unit){
+    this.addTextChangedListener(object: TextWatcher {
+
+        override fun afterTextChanged(editable: Editable?) {
+            completion(editable)
+        }
+
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+        }
+
+        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+        }
+
+    })
+}
