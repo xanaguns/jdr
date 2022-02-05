849c21e7428d82f59145a5ec0fee68126726244c
gschoi <gunsoo.choi@bluebirdcorp.com>
2022-02-05 13:40:10 +0900

    add jetpack 03_bottomNav_with_navigation-master project

---
+++ android_jetpack/03_bottomNav_with_navigation-master/app/src/main/java/com/example/bottomnavigationviewwithnavigation/fragments/FriendsFragment.kt
@@ -0,0 +1,33 @@
+package com.example.bottomnavigationviewwithnavigation.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.bottomnavigationviewwithnavigation.databinding.FragmentFriendsBinding
+import com.example.bottomnavigationviewwithnavigation.databinding.FragmentHomeBinding
+
+class FriendsFragment : Fragment() {
+
+    private var mBinding : FragmentFriendsBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val binding = FragmentFriendsBinding.inflate(inflater, container, false)
+
+        mBinding = binding
+
+        return mBinding?.root
+    }
+
+    override fun onDestroyView() {
+        mBinding = null
+        super.onDestroyView()
+    }
+
+}
---
+++ android_jetpack/03_bottomNav_with_navigation-master/app/src/main/java/com/example/bottomnavigationviewwithnavigation/fragments/MusicFragment.kt
@@ -0,0 +1,34 @@
+package com.example.bottomnavigationviewwithnavigation.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.bottomnavigationviewwithnavigation.databinding.FragmentFriendsBinding
+import com.example.bottomnavigationviewwithnavigation.databinding.FragmentHomeBinding
+import com.example.bottomnavigationviewwithnavigation.databinding.FragmentMusicBinding
+
+class MusicFragment : Fragment() {
+
+    private var mBinding : FragmentMusicBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val binding = FragmentMusicBinding.inflate(inflater, container, false)
+
+        mBinding = binding
+
+        return mBinding?.root
+    }
+
+    override fun onDestroyView() {
+        mBinding = null
+        super.onDestroyView()
+    }
+
+}
